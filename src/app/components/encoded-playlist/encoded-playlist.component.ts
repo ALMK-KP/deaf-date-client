b0c516fcb7acab6ae7f4d8f36cf43d3afb5a2300
asenataluczak <asenata.luczak@gmail.com>
2024-07-16 18:13:31 +0200

    encoded playlist layout

--- src/app/components/encoded-playlist/encoded-playlist.component.ts
+++ src/app/components/encoded-playlist/encoded-playlist.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TracksService } from '../../services/tracks.service';
+import { TrackListComponent } from '../track-list/track-list.component';
 
 @Component({
   selector: 'app-encoded-playlist',
   standalone: true,
-  imports: [],
+  imports: [TrackListComponent],
   templateUrl: './encoded-playlist.component.html',
 })
-export class EncodedPlaylistComponent {
+export class EncodedPlaylistComponent implements OnInit {
+  private readonly tracksService = inject(TracksService);
+  tracks = [];
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    const playlistId = this.route.snapshot.paramMap.get('id');
+    if (playlistId) {
+      this.tracksService
+        .getPlaylist(playlistId, 'ENCODED')
+        .subscribe((val: any) => {
+          this.tracks = val.data;
+        });
+    }
+  }
 }
--- src/app/components/track-list/track-list.component.ts
+++ src/app/components/track-list/track-list.component.ts
@@ -8,4 +8,16 @@ import { Component, Input } from '@angular/core';
 })
 export class TrackListComponent {
   @Input() tracks: any = [];
+  @Input() encoded = false;
+  expanded = false;
+  expandedTrackId: number | null = null;
+
+  toggleExpanded(trackId: number) {
+    if (trackId !== this.expandedTrackId && this.expanded) {
+      this.expanded = true;
+    } else {
+      this.expanded = !this.expanded;
+    }
+    this.expandedTrackId = trackId;
+  }
 }
