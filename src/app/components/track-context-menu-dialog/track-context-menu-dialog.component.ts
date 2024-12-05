47083dd36b28faf274c5af03e40721a62a422127
asenataluczak <asenata.luczak@gmail.com>
2024-12-05 22:37:56 +0100

    inline editing of custom title - in progress

--- src/app/components/track-context-menu-dialog/track-context-menu-dialog.component.ts
+++ src/app/components/track-context-menu-dialog/track-context-menu-dialog.component.ts
@@ -13,18 +13,31 @@ import { DialogService } from '../../services/dialog.service';
 import { PolymorpheusTemplate } from '@taiga-ui/polymorpheus';
 import { TuiButton } from '@taiga-ui/core';
 import { GlobalStore } from '../../global.store';
+import { TuiInputInline } from '@taiga-ui/kit';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { TuiAutoFocus } from '@taiga-ui/cdk';
 
 @Component({
   selector: 'app-track-context-menu-dialog',
   standalone: true,
   templateUrl: './track-context-menu-dialog.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [TuiSheetDialog, PolymorpheusTemplate, TuiButton],
+  imports: [
+    TuiSheetDialog,
+    PolymorpheusTemplate,
+    TuiButton,
+    TuiInputInline,
+    ReactiveFormsModule,
+    TuiAutoFocus,
+    FormsModule,
+  ],
 })
 export class TrackContextMenuDialogComponent {
   options: Partial<TuiSheetDialogOptions<any>>;
   isOpened = false;
   isConfirmationMode = false;
+  isEditingMode = false;
+  customTitleControl = new FormControl();
 
   @Output() removeTrackEmitter = new EventEmitter();
 
@@ -51,7 +64,14 @@ export class TrackContextMenuDialogComponent {
     // this.dialogHelper.confirmDialogAction$.next(this.options.data!.actionType);
   }
 
+  onBlur(): void {
+    this.isEditingMode = false;
+    // this.saveHeading(this.heading);
+  }
+
   dialogClosed() {
     this.isConfirmationMode = false;
   }
+
+  protected readonly blur = blur;
 }
