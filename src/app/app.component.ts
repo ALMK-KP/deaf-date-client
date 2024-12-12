2879912e027d02806692156b50aa7e5537eff77d
asenataluczak <asenata.luczak@gmail.com>
2024-12-12 22:45:28 +0100

    removed different color mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { TuiRoot } from '@taiga-ui/core';
 import {
-  AfterViewInit,
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
   inject,
-  OnInit,
-  signal,
-  ViewChild,
 } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { WebsocketsService } from './shared/services/websockets.service';
 import { ConfirmDialogComponent } from './shared/components/confirm-dialog/confirm-dialog.component';
 import { TargetDirective } from './shared/directives/target.directive';
@@ -18,7 +14,6 @@ import { CreatePlaylistModule } from './modules/create-playlist/create-playlist.
 import { ViewPlaylistModule } from './modules/view-playlist/view-playlist.module';
 import { TuiElasticSticky } from '@taiga-ui/addon-mobile';
 import { tuiClamp } from '@taiga-ui/cdk';
-import { distinctUntilChanged, map, Observable, startWith } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 
 @Component({
@@ -41,17 +36,13 @@ import { AsyncPipe } from '@angular/common';
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent implements OnInit {
-  orangeMode = false;
+export class AppComponent {
   connectedUsers: number;
   scale = 1;
 
   websocketsService = inject(WebsocketsService);
 
-  constructor(
-    private router: Router,
-    private cdr: ChangeDetectorRef,
-  ) {
+  constructor(private cdr: ChangeDetectorRef) {
     this.websocketsService.connectedUsersChange$.subscribe((val: any) => {
       this.connectedUsers = val - 1;
       this.cdr.markForCheck();
@@ -62,11 +53,5 @@ export class AppComponent implements OnInit {
   onElastic(scale: number) {
     this.scale = tuiClamp(scale, 0.7, 1);
     this.cdr.detectChanges();
-  }
-
-  ngOnInit() {
-    this.router.events.subscribe(() => {
-      this.orangeMode = this.router.url.includes('/decoded');
-    });
   }
 }
