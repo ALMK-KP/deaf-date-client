4fa06f19b1b1a85fb423b3893b669b30672ff343
asenataluczak <asenata.luczak@gmail.com>
2024-12-14 21:05:13 +0100

    styles refactoring - structure, colors, sass

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      primary: "#4ac99b",
+      "primary-selection": "rgba(74, 201, 155, 0.4)",
+      "gray-darker": "#222222",
+      "gray-dark": "#292929",
+      gray: "#383838",
+      "gray-light": "#464646",
+      "gray-lighter": "#a3a3a3",
+      red: "#ff8c67",
+      white: "#e5e5e5",
+      transparent: 'transparent'
+    },
   },
   plugins: [],
 }
